e707366ce6845a576fab8b7593d819c8808266c9
Christoph Franke <christoph.franke@web.de>
2015-07-15 15:51:17 +0200

    zoomable map

--- public/js/app/views/visuals/Map.js
+++ public/js/app/views/visuals/Map.js
@@ -29,13 +29,51 @@ var Map = function(options){
 
         var innerwidth = this.width - margin.left - margin.right,
             innerheight = this.height - margin.top - margin.bottom ;     
-        
+            
+        var projection = d3.geo.mercator()
+            .center([10.5, 51.35])
+            .scale(2000)
+            .translate([innerwidth / 2, innerheight / 2]);
+
+        var path = d3.geo.path()
+            .projection(projection);
+
+        var zoom = d3.behavior.zoom()
+            .translate(projection.translate())
+            .scale(projection.scale())
+            .scaleExtent([this.height, 150 * this.height]) //max zoom 150
+            .on("zoom", zoomed);
+
+        var mouseover = function(d, i) {
+            var tooltip = d3.select('body').append("div").attr("class", "tooltip");
+            var bar = d3.select(this);
+            bar.classed("highlight", true);
+            tooltip.style("opacity", .9);     
+            //var parent = d3.select(this.parentNode); 
+            tooltip.html(d.properties.name);
+
+            tooltip.style("left", (d3.event.pageX + 10) + "px")     
+                   .style("top", (d3.event.pageY - parseInt(tooltip.style("height"))) + "px"); 
+        };     
+
+        var mouseout = function(){
+            d3.select(this).classed("highlight", false);     
+            d3.select('body').selectAll("div.tooltip").remove();
+            };
+            
         var svg = d3.select(this.el).append('svg')
             .attr('xmlns', "http://www.w3.org/2000/svg")
             .attr('xmlns:xmlns:xlink', "http://www.w3.org/1999/xlink")
             .attr('width', this.width )
             .attr('height', this.height);  
     
+        var g = svg.append("g")
+        .call(zoom);
+
+        g.append("rect")
+            .attr("class", "background")
+            .attr("width", innerwidth)
+            .attr("height", innerheight);
 
         d3.json(this.source, function(error, map) {
             if (error) return console.error(error);
@@ -44,33 +82,11 @@ var Map = function(options){
                 return _this.units.indexOf( el.id ) >= 0;
             });
             
-            var projection = d3.geo.mercator()
-                .center([10.5, 51.35])
-                .scale(2000)
-                .translate([innerwidth / 2, innerheight / 2]);
-        
-            var path = d3.geo.path()
-                .projection(projection);
-        
-            var mouseover = function(d, i) {
-                var tooltip = d3.select('body').append("div").attr("class", "tooltip");
-                var bar = d3.select(this);
-                bar.classed("highlight", true);
-                tooltip.style("opacity", .9);     
-                //var parent = d3.select(this.parentNode); 
-                tooltip.html(d.properties.name);
-
-                tooltip.style("left", (d3.event.pageX + 10) + "px")     
-                       .style("top", (d3.event.pageY - parseInt(tooltip.style("height"))) + "px"); 
-            };     
-
-            var mouseout = function(){
-                d3.select(this).classed("highlight", false);     
-                d3.select('body').selectAll("div.tooltip").remove();
-            };
         
             // FEATURE-SHAPES
-            svg.selectAll(".subunit")
+            g.append("g")
+                .attr("id", "Deutschland")
+              .selectAll(".subunit")
                 .data(topojson.feature(map, subunits).features)
               .enter().append("path")
                 .attr("class", function(d) { return "subunit rs" + d.id; })
@@ -79,17 +95,29 @@ var Map = function(options){
                 .on("mouseout", mouseout);
         
             // INTERIOR BOUNDARIES
-            svg.append("path")
+            g.append("path")
                 .datum(topojson.mesh(map, subunits, function(a, b) {return a !== b }))
                 .attr("d", path)
                 .attr("class", "subunit-boundary");        
         
+            // OUTER BOUNDARY
+            g.append("path")
+                .datum(topojson.mesh(map, subunits, function(a, b) {return a === b }))
+                .attr("d", path)
+                .attr("class", "outer-boundary");    
+        
             if(callback)
                 callback(this.el.innerHTML);
+            
         });
         
+        function zoomed() {
+            projection.translate(d3.event.translate).scale(d3.event.scale);
+            g.selectAll("path").attr("d", path);
+        }
+        
         function translation(x,y) {
-          return 'translate(' + x + ',' + y + ')';
+            return 'translate(' + x + ',' + y + ')';
         }
     };
     
